c5cd6f58939bcbdb8963c7eb633559c51988c21a
jose adrian buctuanon <jbuctuano@dnamicro.com>
2019-09-24 05:35:48 -0700

    updating to 3

--- src/Card.js
+++ src/Card.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
 import './Card.css';
 
-function Card({ name, image , birth_year, homeworld }) {
-  return (
-    <div className='card'>
-      <div className='card-content'>
-          <div className='card-name'>{name}</div>
-          <img src={`http://localhost:3008/${image}`} alt='profile'/>
-          <p>
-              <span>Birthday:</span>
-              <span>{ birth_year }</span>
-          </p>
-          <p>
-              {/* Note that in order to get the homeworld's name, you have to get the planet name from a different endpoint than the people */}
-              <span>Homeworld:</span>
-              <span>{homeworld}</span>
-          </p>
+class Card extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isEdit: false
+    }
+  }
+
+  render() {
+    const { name, image , birth_year, homeworld } = this.props
+    return (
+      <div className='card'>
+        <div className='card-content'>
+            <div className='card-name'>{name}</div>
+            <img src={`http://localhost:3008/${image}`} alt='profile'/>
+            <p>
+                <span>Birthday:</span>
+                <span>{ birth_year }</span>
+            </p>
+            <p>
+                {/* Note that in order to get the homeworld's name, you have to get the planet name from a different endpoint than the people */}
+                <span>Homeworld:</span>
+                <span>{homeworld}</span>
+            </p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Card;
